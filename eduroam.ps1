78d3a111a0dfab186c60d9448ff89fbd8de8186d
unknown <jda-silva3@int.ac-nancy-metz.fr>
2023-03-16 14:06:49 +0100

    auto_install_eduroam

--- eduroam.ps1
+++ eduroam.ps1
@@ -3,13 +3,13 @@ netsh wlan delete profile name="eduroam® via partner"
 
 Start-Sleep -Milliseconds 100
 
-$eduroam_temp = "C:\temp\eduroam.exe"
+$eduroam = "C:\temp\eduroam.exe"
 $dest = "https://cat.eduroam.org/user/API.php?action=downloadInstaller&lang=fr&profile=5886&device=w10&generatedfor=user&openroaming=0"
 $proxy = ([System.Net.WebRequest]::GetSystemWebproxy()).GetProxy($dest)
 
-Invoke-WebRequest -Uri $dest -Proxy $proxy -ProxyUseDefaultCredentials -OutFile $eduroam_temp | Wait-Process
+Invoke-WebRequest -Uri $dest -Proxy $proxy -ProxyUseDefaultCredentials -OutFile $eduroam | Wait-Process
 
-Start-Process $eduroam_temp | Wait-Process
+Start-Process $eduroam | Wait-Process
 
 netsh wlan set profileparameter name="eduroam®" ConnectionMode=auto
 netsh wlan set profileorder name="eduroam®" priority=1 interface="Wi-Fi"
