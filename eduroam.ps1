dafff8a8eac1ba1ece3dda4aff4ded8ed8b894cb
unknown <jda-silva3@int.ac-nancy-metz.fr>
2023-03-16 14:05:49 +0100

    Update eduroam.ps1

--- eduroam.ps1
+++ eduroam.ps1
@@ -1,11 +1,15 @@
-<# [string[]]$netsh = netsh wlan show networks | Select-String -Pattern "SSID" 
-$netsh | ForEach-Object {[string]$_.Substring(9, $_.Length-9)} | Where-Object {$_} #>
-
 netsh wlan delete profile name="eduroam®"
 netsh wlan delete profile name="eduroam® via partner"
 
-Start-Process .\eduroam.exe
+Start-Sleep -Milliseconds 100
 
+$eduroam_temp = "C:\temp\eduroam.exe"
+$dest = "https://cat.eduroam.org/user/API.php?action=downloadInstaller&lang=fr&profile=5886&device=w10&generatedfor=user&openroaming=0"
+$proxy = ([System.Net.WebRequest]::GetSystemWebproxy()).GetProxy($dest)
+
+Invoke-WebRequest -Uri $dest -Proxy $proxy -ProxyUseDefaultCredentials -OutFile $eduroam_temp | Wait-Process
+
+Start-Process $eduroam_temp | Wait-Process
 
 netsh wlan set profileparameter name="eduroam®" ConnectionMode=auto
 netsh wlan set profileorder name="eduroam®" priority=1 interface="Wi-Fi"
